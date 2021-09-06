386b5e2464d59eb051784fb1b40973bf4cbdd88f
Twofold-One <evan_geras@mail.ru>
2021-09-07 00:38:36 +0700

    display and clear functions added

--- scripts/main.js
+++ scripts/main.js
@@ -33,9 +33,52 @@ function operate(operator, num1 , num2) {
     return result;
 }
 
-//Populate display function
+//Populate display with numbers function
 function displayNumbers() {
     const display = document.querySelector('#calculator-display');
     const numButtons = document.querySelectorAll('#button-number');
-    
+
+    numButtons.forEach((button) => {
+        button.addEventListener('click', function(e) {
+            if (display.textContent == 0) {
+                display.textContent = e.target.textContent
+            } else {
+                display.textContent += e.target.textContent;
+            }    
+        });
+    });
 }
+displayNumbers();
+
+//Clear display function
+function clearDisplay() {
+    const display = document.querySelector('#calculator-display');
+    const clearButton = document.querySelector('#clear-button');
+
+    clearButton.addEventListener('click', () => {
+        display.textContent = 0;
+    });
+}
+clearDisplay();
+
+//One dot function
+function oneDot() {
+    const display = document.querySelector('#calculator-display');
+    const dotButton = document.querySelector('.dot-btn');
+    if (display.textContent.indexOf('.') !== -1) {
+        dotButton.disabled = true;
+    } else {
+        dotButton.disabled = false;
+    }   
+}
+oneDot();
+
+
+//Global stored operator, num1, num2 array
+const displayValue = {
+    num1: 0,
+    operator: undefined,
+    num2: 0
+}
+
+
