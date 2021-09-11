dbec9bd4d9012d16cc3855d11d102048c7b36d3b
Twofold-One <evan_geras@mail.ru>
2021-09-12 00:58:33 +0700

    basic logic of calculations on operator and equality buttons push is working

--- scripts/main.js
+++ scripts/main.js
@@ -33,11 +33,19 @@ function operate(operator, num1 , num2) {
     return result;
 }
 
+//Global storing obj for operator, num1, num2 
+const storedValue = {
+    num1: null,
+    operator: null,
+    num2: null,
+    result: null
+}
+
 //Populate display with numbers function
 function displayNumbers() {
     const display = document.querySelector('#calculator-display');
     const numButtons = document.querySelectorAll('#button-number');
-    const dotButton = document.querySelector('#button-dot');
+    const dotButton = document.querySelector('#button-dot'); 
 
     //One dot function
     function oneDot() {
@@ -60,7 +68,9 @@ function displayNumbers() {
 
     numButtons.forEach((button) => {
         button.addEventListener('click', function(e) {
-            if (display.textContent.includes('0.') === true) {
+            if (display.textContent.includes(String(storedValue.num1)) || display.textContent.includes(String(storedValue.result))) {
+                display.textContent = e.target.textContent;
+            } else if (display.textContent.includes('0.') === true) {
                 display.textContent += e.target.textContent;
             } else if (display.textContent.indexOf(0) === 0) {
                 display.textContent = e.target.textContent;
@@ -73,11 +83,81 @@ function displayNumbers() {
 }
 displayNumbers();
 
+//function which stores num1 and operator in displayValue on operator click
+function operatorClick() {
+    const display = document.querySelector('#calculator-display');
+    const operatorButton = document.querySelectorAll('#button-operator');
+
+        operatorButton.forEach((button) => {
+            button.addEventListener('click', function(e) {
+                if (storedValue.num1 !== null && storedValue.operator !== null) {
+                    storedValue.num2 = Number(display.textContent);
+                    storedValue.result = operate(storedValue.operator, storedValue.num1, storedValue.num2);
+                    display.textContent = storedValue.result;
+                    storedValue.operator = e.target.textContent;
+                    storedValue.num1 = null;
+                    storedValue.num2 = null;
+                    console.log(storedValue);  
+                } else if (storedValue.result !== null && storedValue.operator !== null) {
+                    storedValue.num1 = storedValue.result;
+                    storedValue.num2 = Number(display.textContent);
+                    storedValue.result = operate(storedValue.operator, storedValue.num1, storedValue.num2);
+                    display.textContent = storedValue.result;
+                    storedValue.operator = e.target.textContent;
+                    storedValue.num1 = null;
+                    storedValue.num2 = null;
+                    console.log(storedValue);
+                } else {
+                    storedValue.num1 = Number(display.textContent);
+                    storedValue.operator = e.target.textContent;
+                    console.log(storedValue);   
+                }             
+            });
+        });
+}
+operatorClick();
+
+
+//function which stores num2 and runs equality operation
+function equalityClick() {
+    const display = document.querySelector('#calculator-display');
+    const equalityButton = document.querySelector('#button-equality');
+
+
+    equalityButton.addEventListener('click', function() {
+        if (storedValue.num1 === null && storedValue.operator === null && storedValue.num2 === null && storedValue.result === null) {
+            display.textContent; 
+        } else if (storedValue.result !== null && storedValue.operator === null) {
+            display.textContent = storedValue.result;
+            console.log(storedValue);
+        } else if (storedValue.result !== null) {
+            storedValue.num1 = storedValue.result;
+            storedValue.num2 = Number(display.textContent);
+            storedValue.result = operate(storedValue.operator, storedValue.num1, storedValue.num2);
+            display.textContent = storedValue.result;
+            storedValue.num1 = null;
+            storedValue.num2 = null;
+            storedValue.operator = null;
+            console.log(storedValue);
+        } else {
+            storedValue.num2 = Number(display.textContent);
+            storedValue.result = operate(storedValue.operator, storedValue.num1, storedValue.num2);
+            display.textContent = storedValue.result;
+            storedValue.num1 = null;
+            storedValue.num2 = null;
+            storedValue.operator = null;
+            console.log(storedValue);
+        }
+    });
+}
+equalityClick();
+
+
 //Clear display function
 function clearDisplay() {
     const display = document.querySelector('#calculator-display');
     const clearButton = document.querySelector('#clear-button');
-    const dotButton = document.querySelector('#button-dot');
+    const dotButton = document.querySelector('#button-dot'); 
 
     clearButton.addEventListener('click', () => {
         display.textContent = 0;
@@ -86,44 +166,29 @@ function clearDisplay() {
 }
 clearDisplay();
 
-//Global storing obj for operator, num1, num2 
-const storedValue = {
-    num1: undefined,
-    operator: undefined,
-    num2: undefined
-}
-
-//function which stores num1 and operator in displayValue on operator click
-function storeFirstNumAndOperator() {
+//All clear function
+function allClear() {
     const display = document.querySelector('#calculator-display');
-    const operatorButton = document.querySelectorAll('#button-operator');
+    const allClearButton = document.querySelector('#all-clear-button');
+    const dotButton = document.querySelector('#button-dot'); 
 
-    operatorButton.forEach((button) => {
-        button.addEventListener('click', function(e) {
-            storedValue.num1 = Number(display.textContent);
-            storedValue.operator = e.target.textContent;
-            console.log(storedValue);
-        });
+    allClearButton.addEventListener('click', () => {
+        display.textContent = 0;
+        dotButton.disabled = false;
+        storedValue.num1 = null;
+        storedValue.num2 = null;
+        storedValue.operator = null;
+        storedValue.result = null;
     });
 }
-storeFirstNumAndOperator();
+allClear();
 
-//function which stores num2 and runs equality operation
-function storeSecondNumAndOperate() {
+// ??????? Alert on deviding on zero
+function divideByZeroAlert() { 
     const display = document.querySelector('#calculator-display');
-    const equalityButton = document.querySelector('#button-equality');
-
-    equalityButton.addEventListener('click', function() {
-        storedValue.num2 = Number(display.textContent);
-        display.textContent = operate(storedValue.operator, storedValue.num1, storedValue.num2);
-        console.log(storedValue);
-        console.log(operate(storedValue.operator, storedValue.num1, storedValue.num2))
-    });
+    if (display.textContent === Infinity) {
+        alert('You can\'t divide by 0!');
+        storedValue.result = null;
+    }       
 }
-storeSecondNumAndOperate();
-
-//function which clears display if num1 is already stored
-function storedNumCheck() {
-    if (storedValue.num1 !== undefined) {
-    }
-}
+divideByZeroAlert();
